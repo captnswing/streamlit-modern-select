ee3c469eebb76c25ab56f881ffd49fa4964d6dbc
Frank Hoffsümmer <frank.hoffsummer@easypark.net>
2024-03-09 22:19:53 +0100

    working select with size

--- modern_select/__init__.py
+++ modern_select/__init__.py
@@ -38,40 +38,6 @@ else:
     _component_func = components.declare_component("modern_select", path=build_dir)
 
 
-# Create a wrapper function for the component. This is an optional
-# best practice - we could simply expose the component function returned by
-# `declare_component` and call it done. The wrapper allows us to customize
-# our component's API: we can pre-process its input args, post-process its
-# output value, and add a docstring for users.
-def modern_select(name, key=None):
-    """Create a new instance of "my_component".
-
-    Parameters
-    ----------
-    name: str
-        The name of the thing we're saying hello to. The component will display
-        the text "Hello, {name}!"
-    key: str or None
-        An optional key that uniquely identifies this component. If this is
-        None, and the component's arguments are changed, the component will
-        be re-mounted in the Streamlit frontend and lose its current state.
-
-    Returns
-    -------
-    int
-        The number of times the component's "Click Me" button has been clicked.
-        (This is the value passed to `Streamlit.setComponentValue` on the
-        frontend.)
-
-    """
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    #
-    # "default" is a special argument that specifies the initial return
-    # value of the component before the user has interacted with it.
-    component_value = _component_func(name=name, key=key, default=0)
-
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
+def modern_select(options, size=5, key=None):
+    component_value = _component_func(options=options, size=size, key=key, default="")
     return component_value
--- modern_select/frontend/src/ModernSelect.tsx
+++ modern_select/frontend/src/ModernSelect.tsx
@@ -1,84 +1,55 @@
 import {
-  Streamlit,
-  StreamlitComponentBase,
-  withStreamlitConnection,
+    Streamlit,
+    StreamlitComponentBase,
+    withStreamlitConnection,
 } from "streamlit-component-lib"
-import React, { ReactNode } from "react"
+import React, {ReactNode} from "react"
 
 interface State {
-  numClicks: number
-  isFocused: boolean
+    selectedOption: string
+    isFocused: boolean
 }
 
-/**
- * This is a React-based component template. The `render()` function is called
- * automatically when your component should be re-rendered.
- */
 class ModernSelect extends StreamlitComponentBase<State> {
-  public state = { numClicks: 0, isFocused: false }
+    public state = {selectedOption: '', isFocused: false}
 
-  public render = (): ReactNode => {
-    // Arguments that are passed to the plugin in Python are accessible
-    // via `this.props.args`. Here, we access the "name" arg.
-    const name = this.props.args["name"]
-
-    // Streamlit sends us a theme object via props that we can use to ensure
-    // that our component has visuals that match the active theme in a
-    // streamlit app.
-    const { theme } = this.props
-    const style: React.CSSProperties = {}
-
-    // Maintain compatibility with older versions of Streamlit that don't send
-    // a theme object.
-    if (theme) {
-      // Use the theme object to style our button border. Alternatively, the
-      // theme style is defined in CSS vars.
-      const borderStyling = `1px solid ${
-        this.state.isFocused ? theme.primaryColor : "gray"
-      }`
-      style.border = borderStyling
-      style.outline = borderStyling
+    public render = (): ReactNode => {
+        // Arguments that are passed to the plugin in Python are accessible
+        // via `this.props.args`.
+        const options = this.props.args["options"]
+        const size = this.props.args["size"]
+        return (
+            <span>
+        Hello!<br/>
+        <select
+            size={size}
+            onChange={this.onSelectionChange}
+        >
+            {options.map((option: string) => (
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
+      </span>
+        )
     }
 
-    // Show a button and some text.
-    // When the button is clicked, we'll increment our "numClicks" state
-    // variable, and send its new value back to Streamlit, where it'll
-    // be available to the Python program.
-    return (
-      <span>
-        Hello, {name}! &nbsp;
-        <button
-          style={style}
-          onClick={this.onClicked}
-          disabled={this.props.disabled}
-          onFocus={this._onFocus}
-          onBlur={this._onBlur}
-        >
-          Click Me!
-        </button>
-      </span>
-    )
-  }
+    private onSelectionChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+        const selectedOption = event.currentTarget.value;
+        this.setState(
+            {selectedOption: selectedOption},
+            () => Streamlit.setComponentValue(this.state.selectedOption)
+        )
+    }
 
-  /** Click handler for our "Click Me!" button. */
-  private onClicked = (): void => {
-    // Increment state.numClicks, and pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
-    this.setState(
-      prevState => ({ numClicks: prevState.numClicks + 1 }),
-      () => Streamlit.setComponentValue(this.state.numClicks)
-    )
-  }
+    private _onFocus = (): void => {
+        this.setState({isFocused: true})
+    }
 
-  /** Focus handler for our "Click Me!" button. */
-  private _onFocus = (): void => {
-    this.setState({ isFocused: true })
-  }
-
-  /** Blur handler for our "Click Me!" button. */
-  private _onBlur = (): void => {
-    this.setState({ isFocused: false })
-  }
+    private _onBlur = (): void => {
+        this.setState({isFocused: false})
+    }
 }
 
 // "withStreamlitConnection" is a wrapper function. It bootstraps the
