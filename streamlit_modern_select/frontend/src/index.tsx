7c6690b1926581a1fd063fb548d9b23041dc5f46
Frank Hoffsümmer <frank.hoffsummer@easypark.net>
2024-03-10 15:25:01 +0100

    update to latest react

--- streamlit_modern_select/frontend/src/index.tsx
+++ streamlit_modern_select/frontend/src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 import ModernSelect from "./ModernSelect"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ModernSelect />
-  </React.StrictMode>,
-  document.getElementById("root")
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    <React.StrictMode>
+        <ModernSelect/>
+    </React.StrictMode>,
 )
